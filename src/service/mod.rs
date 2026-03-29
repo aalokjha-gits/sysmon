9811504ae3608c8ae9d43f9335ec2b5f68e7bee8
Aalok Jha <aalokaj59@gmail.com>
2026-03-29 23:12:00 +0530

    fix: cargo fmt ordering in service/mod.rs

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,12 +1,12 @@
-#[cfg(target_os = "macos")]
-mod macos;
 #[cfg(target_os = "linux")]
 mod linux;
-
 #[cfg(target_os = "macos")]
-pub use macos::*;
+mod macos;
+
 #[cfg(target_os = "linux")]
 pub use linux::*;
+#[cfg(target_os = "macos")]
+pub use macos::*;
 
 // Fallback for unsupported platforms
 #[cfg(not(any(target_os = "macos", target_os = "linux")))]
